74e92c516e70f336f8a3215b75e4ccab09ec97a7
Matthew Webb <mwebbmwebb@gmail.com>
2019-10-05 22:11:11 -0500

    unhide mouse on end screens

--- Button.gd
+++ Button.gd
@@ -6,6 +6,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
