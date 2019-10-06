122e7a93c0ed8e53787ee53648bcdcfb18742c87
Matthew Webb <mwebbmwebb@gmail.com>
2019-10-05 22:05:12 -0500

    readd ESCAPE for quit & capture mouse

--- GUI.gd
+++ GUI.gd
@@ -6,6 +6,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_parent().get_parent().get_child(5).get_child(1).connect("coin_collected", self, "increment_coin_label")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
