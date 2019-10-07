b1294b2950b4f1ba3aeb3170aa9b0d15b4e1f01c
Matthew Webb <mwebbmwebb@gmail.com>
2019-10-06 23:03:18 -0500

    convert kinematicbody script to player C++

--- code/scripts/gdlibrary.cpp
+++ code/scripts/gdlibrary.cpp
@@ -3,6 +3,7 @@
 #include "CoinObject/coin_object.h"
 #include "ledge/ledge.h"
 #include "floor_area/floor_area.h"
+#include "player/player.h"
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
 	godot::Godot::gdnative_init(o);
@@ -19,4 +20,5 @@ extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
 	godot::register_class<godot::CoinObject>();
 	godot::register_class<godot::Ledge>();
 	godot::register_class<godot::Floor_Area>();
+	godot::register_class<godot::Player>();
 }
---
+++ code/scripts/player/player.h
@@ -0,0 +1,46 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <Godot.hpp>
+#include <Sprite.hpp>
+#include <KinematicBody.hpp>
+
+namespace godot {
+
+class Player : public KinematicBody {
+	GODOT_CLASS(Player, KinematicBody)
+
+private:
+	static const int SPEED = 10;
+	static const int ACCELERATION = 8;
+	static const int DE_ACCELERATION = 14;
+
+	bool strafe = true;
+
+	double gravity = -9.8 * 3;
+	Vector3 velocity;
+	Transform camera;
+	int jumps = 2;
+	bool prev_collided = false;
+	int coin_count = 0;
+	bool in_ledge = false;
+	int sfx = 1;
+
+public:
+	static void _register_methods();
+
+	static void _init();
+
+	void _ready();
+
+	void hit_floor_received();
+	void leave_floor_received();
+	void leave_ledge_received();
+	void collect_coin();
+	void _process(float);
+	void _physics_process(float);
+};
+
+}
+
+#endif
