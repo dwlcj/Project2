39799583f623736fa62ccd1fe7dd151a7b2f32c6
Ben <ben.hazel@gmail.com>
2019-10-06 21:22:01 -0500

    signals work for windows

--- code/scripts/ledge/ledge.cpp
+++ code/scripts/ledge/ledge.cpp
@@ -5,8 +5,8 @@ using namespace godot;
 void Ledge::_register_methods() {
 	register_method("_on_Ledge_body_entered", &Ledge::_on_Ledge_body_entered);
 	register_method("_on_Ledge_body_exited", &Ledge::_on_Ledge_body_exited);
-	register_signal<Ledge>("hit_ledge");
-	register_signal<Ledge>("leave_ledge");
+	register_signal<Ledge>("hit_ledge", "node", GODOT_VARIANT_TYPE_OBJECT);
+	register_signal<Ledge>("leave_ledge", "node", GODOT_VARIANT_TYPE_OBJECT);
 }
 
 void Ledge::_init() {}
