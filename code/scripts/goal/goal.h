51c77c2fa19fac266e4caea14f9ee7634e2d8ad7
thicctor <vmorandi@utexas.edu>
2019-10-06 22:19:21 -0500

    added goal gdns

---
+++ code/scripts/goal/goal.h
@@ -0,0 +1,27 @@
+#ifndef GOAL_H
+#define GOAL_H
+
+#include <Godot.hpp>
+#include <Sprite.hpp>
+#include <SceneTree.hpp>
+#include <Area.hpp>
+
+
+namespace godot {
+
+	class Goal : public Area {
+		GODOT_CLASS(Goal, Area)
+
+	public:
+		static void _register_methods();
+		static void _init();
+		void _on_Area_body_entered(Variant);
+		 
+
+	};
+
+	
+
+}
+
+#endif
